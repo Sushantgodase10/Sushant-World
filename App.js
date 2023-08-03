7c85d148db81637856e4580d28c300281ea8e0c1
sushant <sushantgodase1048@gmail.com>
2023-08-03 18:14:52 +0530

    Main App

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Sushant World");
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://img.freepik.com/premium-vector/restaurant-logo-design-template_79169-56.jpg" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+// component Compositions
+const Applayout = () => {
+return <div className="app">
+    <Header />
+</div>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<Applayout />);
 
 // time 18.34 seesion 3 rd
+ 
